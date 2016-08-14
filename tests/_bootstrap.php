055d1a4e08d144482df393223bf70884a98962f5
Andrii Vasyliev <sol@hiqdev.com>
2016-08-14 18:52:45 +0000

    minor

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -9,11 +9,11 @@
  * @copyright Copyright (c) 2016, HiQDev (http://hiqdev.com/)
  */
 
-error_reporting(E_ALL & ~E_NOTICE);
+error_reporting(E_ALL);
 
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
-foreach (require __DIR__ . '/../vendor/hiqdev/aliases.php' as $alias => $path) {
+foreach (require __DIR__ . '/../vendor/hiqdev/config/aliases.php' as $alias => $path) {
     Yii::setAlias($alias, $path);
 }
